9ed23a48f30bc569d7b33d03bd839555ff11a7e8
justin <justin2805@gmail.com>
2019-09-30 22:24:14 -0500

    implement simplified miner reward and transactions

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
 const SHA256 = require('crypto-js/sha256');
 
+class Transactions{
+    constructor(fromAddress, toAddress, amount){
+        this.fromAddress = fromAddress;
+        this.toAddress = toAddress;
+        this.amount = amount;
+    }
+}
+
 class Block{
-    constructor(index,timestamp,data,previousHash=""){
-        this.index = index;
+    constructor(timestamp,transactions,previousHash=""){
+        // this.index = index;
         this.timestamp = timestamp;
-        this.data = data;
+        this.transactions = transactions;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
         this.nonce = 0;
     }
 
     calculateHash(){
-        return SHA256(this.index + this.timestamp + this.previousHash + JSON.stringify(this.data) +
+        return SHA256(this.index + this.timestamp + this.previousHash + JSON.stringify(this.transactions) +
         this.nonce).toString();
     }
     
@@ -29,22 +37,65 @@ class BlockChain{
     constructor(){
         this.chain = [this.createGenesisBlock()];
         // increasing difficulty increases time required to mine
-        this.difficulty = 5;
+        this.difficulty = 3;
+        this.pendingTransactions =[];
+        this.miningReward = 100;
     }
     
     createGenesisBlock(){
-        return new Block(0,"01/05/2019","Genesis Block", "0");
+        return new Block("01/05/2019","Genesis Block", "0");
     }
 
     getLatestBlock(){
         return this.chain[this.chain.length -1];
     }
 
-    addBlock(newBlock){
-        newBlock.previousHash = this.getLatestBlock().hash;
-        // newBlock.hash = newBlock.calculateHash();
-        newBlock.mineBlock(this.difficulty)
-        this.chain.push(newBlock);
+    // addBlock(newBlock){
+    //     newBlock.previousHash = this.getLatestBlock().hash;
+    //     // newBlock.hash = newBlock.calculateHash();
+    //     newBlock.mineBlock(this.difficulty)
+    //     this.chain.push(newBlock);
+    // }
+
+    // if mining is successful send the reward to address in parameter
+    minePendingTransactions(miningRewardAddress){
+        // in reality there are multiple pending transactions, and not all of them can be 
+        //included, as block size cannot exceed 1 mb. Miners have to pick which transactions
+        // to include. current program doesnt bother with this
+
+        // the mining reward is added to pending transactions array. It will be sent to you,
+        //  only when you mine the next mining block.
+        let block = new Block(Date.now(),this.pendingTransactions);
+        block.mineBlock(this.difficulty);
+        console.log('Block successfully mined');
+        this.chain.push(block);
+
+        this.pendingTransactions = [
+            new Transactions(null,miningRewardAddress, this.miningReward)
+        ];
+    }
+
+    createTransaction(transaction) {
+        this.pendingTransactions.push(transaction);
+    }
+
+    getBalanceOfAddress(address) {
+        let balance = 0;
+        for(const block of this.chain){
+            // block contains multiple transactions
+            for(const trans of block.transactions){
+                // transferred money from sender. sender address== your address.so balance deducted
+                if(trans.fromAddress === address){
+                    balance -= trans.amount;
+                }
+
+                // transferred money from sender. recvr addrss == your address so balance added from sender
+                if(trans.toAddress === address) {
+                    balance += trans.amount;
+                }
+            }
+        }
+        return balance;
     }
 
     isChainValid(){
@@ -64,6 +115,7 @@ class BlockChain{
 }
 
 let blockChainObj = new BlockChain();
+//implementation 1
 // blockChainObj.addBlock(new Block(1,"02/05/2019",{sender:"SBI", receiver: "Customer1", amt: "1456"}));
 // blockChainObj.addBlock(new Block(2,"03/05/2019",{sender:"CBI", receiver: "Customer2", amt: "458"}));
 // blockChainObj.addBlock(new Block(3,"04/05/2019",{sender:"HDFC", receiver: "Customer3", amt: "7000"}));
@@ -71,17 +123,36 @@ let blockChainObj = new BlockChain();
 // console.log('Is blockchain valid ?', blockChainObj.isChainValid());
 
 // // tampering with blockchain
-// blockChainObj.chain[2].data = {sender:"CBI", receiver: "Customer2", amt: "80000000"}
+// blockChainObj.chain[2].transactions = {sender:"CBI", receiver: "Customer2", amt: "80000000"}
 // console.log('Is blockchain valid ?', blockChainObj.isChainValid());
 
 // // second tampering on same block with hash recalculation
-// blockChainObj.chain[2].data = {sender:"CBI", receiver: "Customer2", amt: "90000000"}
+// blockChainObj.chain[2].transactions = {sender:"CBI", receiver: "Customer2", amt: "90000000"}
 // blockChainObj.chain[2].hash = blockChainObj.chain[2].calculateHash();
 // console.log('Is blockchain valid ?', blockChainObj.isChainValid());
 // // console.log(JSON.stringify(blockChainObj, null, 5));
 
-console.log('Mining block 1');
-blockChainObj.addBlock(new Block(1,"02/05/2019",{sender:"SBI", receiver: "Customer1", amt: "1456"}));
 
-console.log('Mining block 2');
-blockChainObj.addBlock(new Block(2,"03/05/2019",{sender:"CBI", receiver: "Customer2", amt: "458"}));
+
+// implementation 2
+// console.log('Mining block 1');
+// blockChainObj.addBlock(new Block("02/05/2019",{sender:"SBI", receiver: "Customer1", amt: "1456"}));
+
+// console.log('Mining block 2');
+// blockChainObj.addBlock(new Block("03/05/2019",{sender:"CBI", receiver: "Customer2", amt: "458"}));
+
+
+
+// implementation 3
+blockChainObj.createTransaction(new Transactions('address1','address2',100));
+blockChainObj.createTransaction(new Transactions('address2','address1',50));
+
+console.log("\nStarting miner...")
+blockChainObj.minePendingTransactions('SI adress');
+
+console.log('\nBalance of SI is ',blockChainObj.getBalanceOfAddress('SI adress'));
+
+console.log("\nStarting miner again...")
+blockChainObj.minePendingTransactions('SI adress');
+
+console.log('\nBalance of SI is ',blockChainObj.getBalanceOfAddress('SI adress'));
